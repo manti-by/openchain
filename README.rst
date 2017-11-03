5076456f9b6250c844acbff0ccd6efc61f9729cc
Alexander Chaika <manti.by@gmail.com>
2017-11-03 13:36:20 +0300

    Update package setup, readme, license

--- README.rst
+++ README.rst
@@ -1,31 +1,32 @@
 Open Blockchain Library
 =======================
 
-
 About
 -----
 
 Library for creating blockchain networks.
 
-Author: Alex Manti <manti.by@gmail.com>
+Author: Alexander Chaika <manti.by@gmail.com>
 
 Source link: https://bitbucket.org/manti_by/open_blockchain
 
 Requirements:
+
 - Base - ECSDA, LevelDB
 - Development - Flake8
 - Examples - Tornado, PyP2P
     
-    
 Installation
 ------------
 
-From pypi.python.org
+From `PyPi <https://pypi.org/>`_
 
         $ pip install open_blockchain
         
-From [bitbucket.org](https://bitbucket.org/manti_by/open_blockchain)
+From `Bitbucket <https://bitbucket.org/manti_by/open_blockchain>`_
 
         $ git clone git@bitbucket.org:manti_by/open_blockchain.git
+
         $ cd open_blockchain/
+
         $ python setup.py install
--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from setuptools import setup, find_packages
 
 from open_blockchain import __version__
 
-install_reqs = parse_requirements('requirements.txt', session=PipSession())
-reqs = [str(ir.req) for ir in install_reqs]
+install_requirements = parse_requirements('requirements.txt', session=PipSession())
+requirements = [str(ir.req) for ir in install_requirements]
 
 
 def read(file_name):
@@ -22,10 +22,22 @@ setup(
     name='open_blockchain',
     version=__version__,
     description='Open Blockchain Library',
-    long_description=read('README.md'),
-    author='Alex Manti',
+    long_description=read('README.rst'),
+    url='https://bitbucket.org/manti_by/open_blockchain/',
+    author='Alexander Chaika',
     author_email='manti.by@gmail.com',
+    license='BSD',
+    python_requires='>=3.5',
     packages=find_packages(exclude=['*.tests']),
-    install_requires=reqs,
+    install_requires=requirements,
     test_suite='tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: POSIX :: Linux',
+    ],
+    keywords='blockchain',
 )
